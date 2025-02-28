1e7579caaf1982b86b1fe447fc6d38c62499e413
Thalis <thalisbdrago@gmail.com>
2025-02-28 00:07:38 -0300

    submit

--- src/pages/CreateCountdown.jsx
+++ src/pages/CreateCountdown.jsx
@@ -334,7 +334,7 @@ const shareLink = () => {
             style={{ backgroundColor: bgColor, color: textColor, width: "90%", maxWidth: "450px" }}
           >
             <div className="bg-white m-3 text-black w-[80%] mx-auto rounded-md">
-              <span className="">timerSZ.com/{eventTitle.replace(/\s+/g, '-').toLowerCase()}</span>
+              <span className="">timerSZ.com/{eventTitle.replace(/\s+/g, '-').replace(/[^\w\-]/g, '').toLowerCase()}</span>
             </div>
               <img
                 src={image ? image : "https://images.unsplash.com/photo-1624646803808-9c5a9de7aa3f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fGNhc2FsJTIwam92ZW18ZW58MHx8MHx8fDA%3D"}
